9cd165bb708720e25b4ec8bfa1c7a8db4ac3d17f
Arun <arunkumarsingh1980@gmail.com>
2025-01-12 22:38:36 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello JavaScript</title>
+</head>
+<body>
+    <h1 id="message"></h1>
+
+    <script>
+        // Print "Hello" to the console
+        console.log("Hello");
+
+        // Display "Hello" on the webpage
+        document.getElementById("message").textContent = "Hello";
+    </script>
+</body>
+</html>
